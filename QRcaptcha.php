f6f481b9a03de7c791729de7faf1ffca51a87740
Hector Alejandro Parada Blanco <37299769+parablan@users.noreply.github.com>
2026-02-27 13:46:54 -0500

    Update QRcaptcha.php

--- QRcaptcha.php
+++ QRcaptcha.php
@@ -1,11 +1,5 @@
 <?php
-/*
-
-Powered by parablan
-Hector Alejandro Parada Blanco
-Captcha con código QR
-
-*/
+// c:\xampp\htdocs\MAIN2\QRcaptcha.php
 
 require_once 'vendor/autoload.php';
 use chillerlan\QRCode\QRCode;
@@ -18,10 +12,11 @@ if (isset($_SESSION)) {
         'eccLevel' => QRCode::ECC_L,
         'imageBase64' => true,
     ]);
+
     $captcha = rand(1000, 9999);
     $qr_data = (new QRCode($options))->render($captcha);
 
-    // Si es una petición AJAX, devolvemos JSON
+    // Si es una petición AJAX, devolvemos SOLO el JSON limpio
     if (isset($_POST['ajax'])) {
         header('Content-Type: application/json');
         echo json_encode([
@@ -31,53 +26,53 @@ if (isset($_SESSION)) {
         exit;
     }
 
-    // Mostrar como página HTML
-    header('Content-Type: text/html');
-
-    echo ("<script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
-<script>
-        function actualizarQR(){
+    ?>
+    <script>
+        function actualizarQR() {
             $.ajax({
-                url: 'QRcaptcha.php',
+                url: 'index.php', // Apunta a index.php que es quien tiene la sesión
                 type: 'POST',
-                data: { ajax: 1 }, 
+                data: { ajax: 1 },
                 dataType: 'json',
-                success: function(data){
+                success: function (data) {
+                    // Actualizamos la imagen y el campo oculto
                     $('#qr').attr('src', data.qr);
                     $('#captcha').val(data.captcha);
+                    console.log("QR Actualizado: " + data.captcha);
+                },
+                error: function (err) {
+                    console.error("Error al actualizar QR", err);
                 }
             });
         }
 
-        $(document).ready(function(){
-            setInterval(actualizarQR, 30000); 
+        $(document).ready(function () {
+            // Ejecutar cada 30000ms (30 segundos)
+            setInterval(actualizarQR, 30000);
         });
     </script>
-<center>
-<div style='width: 300px; height: 150px; display: flex; align-items: center; justify-content: center; flex-direction: column;'>
-    <h2 style='font-size: 20px; color: #333333;'>
-        Escanea el código QR y digita el número captcha
-    </h2>
-    <small style='font-size: 15px; color: #333333;'>
-        El código QR se actualiza cada 30 segundos
-    </small>
-</div>
-<div style='width: 150px; height: 150px; display: flex; align-items: center; justify-content: center; flex-direction: column;'>
-    <img src='" . $qr_data . "' id='qr' />
-</div>
-<div style='width: 150px; height: 150px; display: flex; align-items: center; justify-content: center; flex-direction: column;'>
-    <form action='validar.php' method='post'>
-        <input type='hidden' id='captcha' name='captcha' value='" . $captcha . "'>
-        <input type='text' placeholder='****' id='captcha_digitado' name='captcha_digitado' style='text-align:center; width:110px; font-weight:bold; font-size:20px; color:#328bd7; background:transparent; letter-spacing: 3px' maxlength='4' autocomplete='off' required>
-        </br>
-        </br>
-        <button type='submit' style='width: 110px; height: 30px; font-weight: bold; font-size: 15px; color: #328bd7; cursor: pointer;'>Enviar</button>
-    </form>
-</div>
-</center>");
-} else {
-    // Sesión no iniciada
+
+    <center>
+        <div style='margin-bottom: 20px;'>
+            <h2 style='font-size: 20px; color: #333;'>Escanea el QR y digita el código</h2>
+            <small style='color: #666;'>El código se actualiza cada 30 segundos</small>
+        </div>
+
+        <div>
+            <img src="<?php echo $qr_data; ?>" id="qr" style="border: 1px solid #ccc; padding: 10px;" />
+        </div>
+
+        <div style='margin-top: 20px;'>
+            <form action='validar.php' method='post'>
+                <input type='hidden' id='captcha' name='captcha' value='<?php echo $captcha; ?>'>
+                <input type='text' placeholder='****' id='captcha_digitado' name='captcha_digitado'
+                    style='text-align:center; width:110px; font-weight:bold; font-size:20px; letter-spacing: 3px'
+                    maxlength='4' autocomplete='off' required>
+                <br><br>
+                <button type='submit' style='cursor: pointer;'>Enviar</button>
+            </form>
+        </div>
+    </center>
+    <?php
 }
-
-
-
+?>
